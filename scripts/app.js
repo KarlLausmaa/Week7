c57fcdc605e1d5cd7f4f55e025907f937bdfb856
Karl Lausmaa <karl.lausmaa@gmai.comgit config --global user.name Karl>
2022-03-15 12:13:27 +0200

    lisa

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,15 @@ const movieTitle = document.querySelector(".userInputTitle");
 const moviePosterURL = document.querySelector(".userInputPoster");
 const btn = document.querySelector(".button");
 const movieTitleToDisplay = document.querySelector(".favoriteMovieTitle");
+const movieYear = document.querySelector(".userInputYear");
+const movieYearToDisplay = document.querySelector(".movieYear");
 
 let titleInStorage = localStorage.getItem('title');
 let imageURLStorage = localStorage.getItem('imageURL');
+let movieYearStorage = localStorage.getItem('movieYear');
 
-if(titleInStorage && imageURLStorage) {
+
+if(titleInStorage && imageURLStorage && movieYearStorage) {
     movieTitleToDisplay.textContent = titleInStorage;
     container.style.backgroundImage = `linear-gradient(rgba(107, 107, 192, 0.63),rgba(70, 66, 66, 0.63)), url('${imageURLStorage}')`;
 }
@@ -15,11 +19,15 @@ if(titleInStorage && imageURLStorage) {
 btn.addEventListener('click', () => {
     let movieTitleInput = movieTitle.value;
     let posterURLInput = moviePosterURL.value;
+    let movieYearInput = movieYear.value;
     localStorage.setItem('title', movieTitleInput);
     localStorage.setItem('imageURL', posterURLInput);
+    localStorage.setItem('movieYear', movieYearInput);
     movieTitleToDisplay.textContent = movieTitleInput;
     container.style.backgroundImage = `linear-gradient(rgba(107, 107, 192, 0.63),rgba(70, 66, 66, 0.63)), url("${posterURLInput}")`;
+    movieYearToDisplay.textContent = movieYearInput;
     movieTitle.value = '';
     moviePosterURL.value = '';
+    movieYear.value = '';
 
 });
